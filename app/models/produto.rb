e68addad289fc86393cb617f5589f351bdc9f04b
Rafael Martins <rafael.martins@corp.globo.com>
2011-01-17 22:07:48 -0200

    segundo dia

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -4,5 +4,12 @@ class Produto < ActiveRecord::Base
   validates_presence_of :preco
   validates_presence_of :dono
   validates_presence_of :email
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
   
+  def to_param
+    "#{id}-#{nome}"
+  end
 end
+
+
+
